6a493944a991a290b3e13c0008bab2080628c80f
munir-2001 <munirabbasi2001@gmail.com>
2025-09-25 17:23:29 +0500

    swagger integration with swagger.ts path definitions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import jupiterRouter from "./routes/jupiterRouter";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./swagger";
 
 const app = express();
 app.use("/api/jupiter", jupiterRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(3000, () => console.log("Server running on port 3000"));
 
